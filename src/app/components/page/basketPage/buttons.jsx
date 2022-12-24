4a4023e2845bfa8e151a3e5ac7d08460a05c6e32
Grechkin Danil <grechkin-danil@mail.ru>
2022-12-24 20:32:10 +0200

    ready basket button bay

--- src/app/components/page/basketPage/buttons.jsx
+++ src/app/components/page/basketPage/buttons.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import styles from "./index.module.css";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
+import { toast } from "react-toastify";
 
 const Buttons = ({ amount }) => {
+    const { user } = useAuth();
     const clearBasket = () => {
         localStorage.setItem("storageBasket", "[]");
         location.reload();
+    };
+    const needAutn = (user) => {
+        if (!user) return toast.error("Чтобы оформить заказ, войти в акаунт");
     };
     return (
         <div className={styles.blockButton}>
@@ -14,9 +20,20 @@ const Buttons = ({ amount }) => {
                 Очистить
             </div>
             <div className={styles.amount}>Сумма: {amount}</div>
-            <Link to="/formPage" className={styles.buttonClick}>
-                <p className={styles.title}>Оформить заказ</p>
-            </Link>
+            {user !== undefined ? (
+                <Link to="/formPage" className={styles.buttonClick}>
+                    <p className={styles.title}>Оформить заказ</p>
+                </Link>
+            ) : (
+                <div
+                    className={styles.buttonClick}
+                    onClick={() => {
+                        needAutn();
+                    }}
+                >
+                    Оформить заказ
+                </div>
+            )}
         </div>
     );
 };
