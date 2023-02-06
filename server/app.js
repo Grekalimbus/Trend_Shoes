baf964ce41c1da745a1639bbdd5f0ad8f7bd6197
Grechkin Danil <grechkin-danil@mail.ru>
2023-02-06 18:14:36 +0200

    add mongoose funktional

--- server/app.js
+++ server/app.js
@@ -1,5 +1,25 @@
 const express = require('express');
 const app = express();
+const config = require('config');
+const chalk = require('chalk');
+const mongoose = require('mongoose');
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: false }));
+
+const PORT = config.get('port') ?? 8080;
+
+async function start() {
+  try {
+    await mongoose.connect(config.get('mongoUri')); // подключение к mongoDB
+    console.log(chalk.green(`Conect MongoDB!!!`));
+    app.listen(PORT, () =>
+      console.log(chalk.green(`Server has been started on port ${PORT} ...`))
+    );
+  } catch (e) {
+    console.log(chalk.red(e.message));
+    process.exit(1);
+  }
+}
+
+start();
